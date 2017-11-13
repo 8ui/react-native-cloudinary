b97f99757a69ea82830c6fe70ba15542ca31a70d
Andrew Sokolov <webdeveloper@my.com>
2017-11-13 11:40:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var { NativeModules } = require('react-native');
 var Sha1 = require('./Sha1');
 
 export default {
@@ -8,21 +7,24 @@ export default {
 
 	upload: function (uri, successCb, errorCb) {
 		var timestamp = Date.now(),
-			keys = "timestamp=" + timestamp + this.options.apiSecret,
-			signature = Sha1.hash( keys ),
-			obj = {
-			    uri: uri,
-			    uploadUrl: "https://api.cloudinary.com/v1_1/" + this.options.cloudName + "/image/upload",
-			    data: {
-			    	api_key: this.options.apiKey,
-			    	timestamp: timestamp,
-			    	signature: signature
-			    }
-			};
+				keys = "timestamp=" + timestamp + this.options.apiSecret,
+				signature = Sha1.hash( keys ),
+				uploadUrl = "https://api.cloudinary.com/v1_1/" + this.options.cloudName + "/image/upload";
 
-		NativeModules.FileTransfer.upload(obj, (err, res) => {
-			if(res) successCb(res);
-		    if(err) errorCb(err);
+		const data = new FormData();
+		data.append('file', {
+			uri,
+			name: 'file',
+			type: 'image/jpeg'
 		});
+		data.append('api_key', this.options.apiKey);
+		data.append('timestamp', timestamp);
+		data.append('signature', signature);
+
+		return fetch(uploadUrl, {
+			  method: 'post',
+			  body: data
+			})
+			.then(r => r.json())
 	}
 };
